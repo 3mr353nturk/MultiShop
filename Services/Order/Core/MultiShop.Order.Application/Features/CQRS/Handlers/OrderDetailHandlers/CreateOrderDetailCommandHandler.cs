0ea1ea346064761d3a995d4f8e0e8d28f4dec9e0
3mr353nturk <esukrusenturk@gmail.com>
2024-07-03 10:37:17 +0300

    Added CQRS and Mediator

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/CreateOrderDetailCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class CreateOrderDetailCommandHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public CreateOrderDetailCommandHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(CreateOrderDetailCommand createOrderDetailCommand)
+        {
+            await _repository.CreateAsync(new OrderDetail
+            {
+                ProductId = createOrderDetailCommand.ProductId,
+                ProductName = createOrderDetailCommand.ProductName,
+                ProductPrice = createOrderDetailCommand.ProductPrice,
+                ProductAmount = createOrderDetailCommand.ProductAmount,
+                ProductTotalPrice = createOrderDetailCommand.ProductTotalPrice,
+                OrderingId = createOrderDetailCommand.OrderingId
+            });
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Handlers/OrderingHandlers/GetOrderingByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Handlers.OrderingHandlers
+{
+    public class GetOrderingByIdQueryHandler : IRequestHandler<GetOrderingByIdQuery, GetOrderingByIdQueryResult>
+    {
+        private readonly IRepository<Ordering> _repository;
+
+        public GetOrderingByIdQueryHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetOrderingByIdQueryResult> Handle(GetOrderingByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetOrderingByIdQueryResult
+            {
+                OrderingId = values.OrderingId,
+                UserId = values.UserId,
+                TotalPrice = values.TotalPrice,
+                OrderDate = values.OrderDate
+            };
+        }
+    }
+}
